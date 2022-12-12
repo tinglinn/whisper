817fc71dd8c65d2f570ba81c07d9749a1ca440be
ting <tinglin0708@gmail.com>
2022-12-11 23:36:57 -0800

    sorting

--- pages/workSession/taskPage.js
+++ pages/workSession/taskPage.js
@@ -10,6 +10,7 @@ import { supabase } from '../../env/supabase';
 import 'react-native-url-polyfill/auto'
 import { LogBox } from 'react-native';
 import DropShadow from "react-native-drop-shadow";
+import { Dropdown } from 'react-native-element-dropdown';
 
 LogBox.ignoreLogs([
     'Non-serializable values were found in the navigation state',
@@ -21,7 +22,7 @@ const renderTask = ( item, navigation ) => (
     <TaskCard task={{ name: item.Title,
         session: { length: item.Minutes, total: item.NumSessions, completed: item.NumSessionsCompleted },
         timeDue: { date: item.Date, isDue: item.IsDue },
-        isActive: item.IsActive
+        isActive: item.IsActive, priority: item.Priority
     }} navigation={navigation} />
   );
 
@@ -46,7 +47,6 @@ function TaskCard({ task, navigation }) {
         text = "start";
     }
     let dueDate = task.timeDue.isDue ? task.timeDue.date : "no due date";
-    
     if (task.isActive) {
         return (
             <DropShadow style={{
@@ -145,29 +145,107 @@ function TaskCard({ task, navigation }) {
         );
     }
 }
-    
+  
+//(t1, t2) => (t1.IsActive && !t2.IsActive) ? -1 : (!t1.isActive && t2.isActive) ? 1 : 0)
+// <MaterialCommunityIcons name="sort-ascending" color={Themes.colors.darkgray} size={40} />
+/*
+function compareActiveFunction(t1, t2) {
+    const active1 = t1.IsActive;
+    const active2 = t2.IsActive;
+    if (active1 && !active2) {
+        return -1
+    } else if (!active1 && active2) {
+        return 1
+    } else {
+        return 0
+    }
+}
+function comparePriorityFunction(t1, t2) {
+
+    if (t1.IsActive && !t2.IsActive) {
+        return -1;
+    }
+    if (!t1.IsActive && t2.IsActive) {
+        return 1;
+    }
+    if (t1.priority == "low" || t2.priority == "high") {
+        return 1;
+    }
+    if (t2.priority == "low" || t1.priority == "high") {
+        return -1;
+    }
+    return 0;
+}
+*/
 export default function TasksOverview({ navigation, route }) {
     const [titles, setTitles] = useState([]) // store data
-
+    const [sortType, setSortType] = useState("active")
+    //const [compareFunction, setCompareFunction] = useState(compareActiveFunction);
+    
     useEffect(() => {
         fetchTasks()
-    }, [titles])
-  
+    }, [titles, sortType])
+
     async function fetchTasks() {
         const {data, error} = await supabase
         .from("Tasks")
-        .select("*")
-        const sortedData = data.sort((t1, t2) => (t1.IsActive && !t2.IsActive) ? -1 : (!t1.isActive && t2.isActive) ? 1 : 0);
+            .select("*")
+        let sortedData = data.sort((t1, t2) => (t1.IsActive && !t2.IsActive) ? -1 : (!t1.isActive && t2.isActive) ? 1 : 0);
+        if (sortType == "priority") {
+            //console.log("updated updated function")
+            sortedData = data.sort(function (t1, t2) {
+                let t1numPriority = 0;
+                let t2numPriority = 0;
+                if (t1.priority == "first") t1numPriority = 1;
+                if (t1.priority == "second") t1numPriority = 2;
+                if (t2.priority == "first") t2numPriority = 1;
+                if (t2.priority == "second") t2numPriority = 2;
+                if (t1.IsActive && !t2.IsActive) {
+                    return -1;
+                }
+                if (!t1.IsActive && t2.IsActive) {
+                    return 1;
+                }
+                return t2numPriority - t1numPriority;
+            });    
+        } else if (sortType == "date due") {
+            sortedData = data.sort(function (t1, t2) {
+                if (t1.IsActive && !t2.IsActive) {
+                    return -1;
+                }
+                if (!t1.IsActive && t2.IsActive) {
+                    return 1;
+                }
+                if (t1.Date == null) {
+                    return 1;
+                }
+                if (t2.Date == null) {
+                    return -1;
+                }
+                return Date.parse(t1.Date) - Date.parse(t2.Date);
+            });  
+        }
         setTitles(sortedData);
     } // populate titles with data
-    
+   //console.log(sortType, titles)
     return (
         <SafeAreaView style={styles.screen}>
             <Header text={'your tasks'} />
 
             <View style={styles.container}>
                 <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
-                    <MaterialCommunityIcons name="sort-ascending" color={Themes.colors.darkgray} size={40} />
+                    <Dropdown
+                        style={styles.dropdown}
+                        data={[{ label: "priority", value: "priority" }, {label: "date due", value: "date due"}]}
+                        placeholder={"sort by"}
+                        placeholderStyle={{ fontFamily: "Poppins" }}
+                        selectedTextStyle={{ fontFamily: "Poppins", fontSize: 14 }}
+                        labelField="label"
+                        valueField="value" 
+                        value={sortType}
+                        onChange={item=>setSortType(item.value)}
+                    />
+                    
                     <AddTaskButton navigation={navigation} />
                 </View>
                 {/* <ScrollView style={{ width: '100%' }}>
@@ -240,5 +318,20 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins',
         fontSize: 18,
         color: Themes.colors.darkgray,
-    }
-});
+    },
+    dropdown: {
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 8,
+        padding: 12,
+        shadowColor: '#000',
+        width: 120,
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+    },
+})
